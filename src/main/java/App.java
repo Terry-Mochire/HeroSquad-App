9ace08687f702cee04837193aa574c28c8c1024c
Terry <terrybmochire@gmail.com>
2022-05-03 12:48:56 +0300

    Create a new form to collect user input and route it.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,12 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        get("/heroes/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "newhero.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
