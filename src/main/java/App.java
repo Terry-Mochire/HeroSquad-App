e5b59e7dfd72af78dd8e05503665aeed27cc3574
Terry <terrybmochire@gmail.com>
2022-05-03 15:18:39 +0300

    Create template that displays details about created hero.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import static spark.Spark.*;
+
+import models.Hero;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import spark.ModelAndView;
 import java.util.ArrayList;
@@ -12,6 +14,23 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "newhero.hbs");
 
+        }, new HandlebarsTemplateEngine());
+
+        post("/heroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String heroName = request.queryParams("name");
+            Integer heroAge = Integer.valueOf(request.queryParams("age"));
+            String heroPower = request.queryParams("superpower");
+            String heroWeakness = request.queryParams("weakness");
+            Hero hero = new Hero(heroName, heroAge, heroPower, heroWeakness);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/heroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getInstances();
+            model.put("hero", hero);
+            return new ModelAndView(model, "allheroes.hbs");
         }, new HandlebarsTemplateEngine());
 
         get("/", (request, response) -> {
