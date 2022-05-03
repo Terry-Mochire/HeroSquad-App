ff236336a0708d9a35839c9ef7da7bf45bf7b56e
Terry <terrybmochire@gmail.com>
2022-05-03 09:07:56 +0300

    Create a landing page and route to it.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,16 @@
 import static spark.Spark.*;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.ModelAndView;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+            }, new HandlebarsTemplateEngine());
+    }
 }
